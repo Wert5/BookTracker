3610d9bc485aec6780c7bce3c4979fb805b6bd8b
Wert5 <grenier.winston@gmail.com>
2015-05-11 17:12:16 -0400

    Fixed File names

---
+++ Starter.java
@@ -0,0 +1,11 @@
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BookData bd=new BookData();
+		BookWindow bw=new BookWindow();
+		bw.setVisible(true);
+	}
+
+}
