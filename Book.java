033152f45cc97c986cb125fdeeb0da7386c9759a
Wert5 <Wert5@users.noreply.github.com>
2015-05-12 09:04:33 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 
 public class Book {
 	private BookOrder store=new BookOrder();
-	private  BookOrder total=new BookOrder();
+	private int total;
 	private ArrayList<BookOrder> orders=new ArrayList<BookOrder>();
 	private double price=0;
 	private String isbn="";
 	private int grade=0;
 	
-	public Book(BookOrder bo,double p,String i,int grd){
+	public Book(BookOrder bo,int tot,double p,String i,int grd){
 		this.store = bo;
-		this.total = bo;
+		this.total = tot;
 		this.price = p;
 		this.isbn = i;
 		this.grade = grd;
@@ -20,7 +20,7 @@ public class Book {
 	public BookOrder getStore() {
 		return store;
 	}
-	public BookOrder getTotal() {
+	public int getTotal() {
 		return total;
 	}
 	public ArrayList<BookOrder> getOrders() {
