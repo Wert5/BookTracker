5021b4832d1f52188c8a2c4db0ec5b028f8c87ae
Wert5 <Wert5@users.noreply.github.com>
2015-05-12 09:59:50 -0400

    Update BookWindow.java

--- BookWindow.java
+++ BookWindow.java
@@ -1,32 +1,47 @@
-import java.awt.BorderLayout;
+
 import java.awt.Frame;
 import java.awt.GraphicsConfiguration;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Window;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 
 public class BookWindow extends JFrame {
+	public SortButtons s=new SortButtons();
+	public SortButtons s2=new SortButtons();
+	
 	public BookWindow(){
 		super();
 		this.setTitle("Books");
 		this.setSize(1000,1000);
 		this.setLocation(10, 10);
-		BorderLayout b=new BorderLayout();
-		
 		JLabel h = new JLabel("Hello");
 		h.setVisible(true);
 		//b.addLayoutComponent(h, BorderLayout.NORTH);
-		this.setLayout(b);
-		this.add(h,BorderLayout.SOUTH);
+		this.setLayout(new GridBagLayout());
+		addComponents();
 		addWindowListener(new WindowAdapter() {
 		  	public void windowClosing(WindowEvent e) {
 			   System.exit(0);
 		  	} //windowClosing
 		} );
 
+	}
+	
+	public void addComponents(){
+		GridBagConstraints c=new GridBagConstraints();
+		c.gridx=1;	
+		c.gridwidth=6;
+		c.gridy=1;
+		this.add(s,c);
+		c.gridy=2;
+		this.add(s2,c);
 	}
 }
