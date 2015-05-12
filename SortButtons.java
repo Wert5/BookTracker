c395519bd477c5f78b6345797abe4f5931a4377b
Wert5 <Wert5@users.noreply.github.com>
2015-05-12 15:56:39 -0400

    Update SortButtons.java

--- SortButtons.java
+++ SortButtons.java
@@ -1,9 +1,14 @@
+import java.awt.Insets;
 import java.awt.LayoutManager;
 import java.awt.Panel;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
+/**
+ * 
+ * @author 154667
+ *This class is the panel with the sorting buttons on it.
+ */
 
 public class SortButtons extends JPanel {
 	public JButton[] sortBut;
@@ -23,14 +28,17 @@ public class SortButtons extends JPanel {
 		sortBut=new JButton[7];
 		sortBut[0]=new JButton("Title");
 		sortBut[1]=new JButton("Total");
-		sortBut[2]=new JButton("Storeroom");
-		sortBut[3]=new JButton("Stored Books");
+		sortBut[2]=new JButton("Room");
+		sortBut[3]=new JButton("Store");
 		sortBut[4]=new JButton("Price");
 		sortBut[5]=new JButton("ISBN");
 		sortBut[6]=new JButton("Grade");
 		for(JButton a:sortBut){
+			a.setMargin(new Insets(0,0,0,0));
 			this.add(a);
 		}
+		sortBut[0].setMargin(new Insets(0,85,0,85));
+		sortBut[2].setMargin(new Insets(0,5,0,5));
 	}
 
 }
