2840b1e346cb1a512405ed7bf6f022c16a94a318
Wert5 <grenier.winston@gmail.com>
2015-05-14 17:01:54 -0400

    fixed most of file reader, almost done

--- BookData.java
+++ BookData.java
@@ -7,22 +7,22 @@ import java.util.ArrayList;
 
 
 public class BookData {
-	int starTemp=0;
-	int count=0;
-	String bookTitle, isbn, grade;
-	int roomNumber,numberbooks;
-	double price;
-	Book.g enumGrade;
 
 	public static final String ORIGINAL="OrigDataBase.txt";
 	public static final String TITLE="TitleData.txt";
-	private static ArrayList<Book> orig = new ArrayList<Book>(50000);
+	private static ArrayList<Book> orig = new ArrayList<Book>(1000);
 	
 	public BookData(){
 		loadData();
 	}
 	
 	public ArrayList<Book> loadData(){
+		int starTemp=0;
+		int count=0;
+		String bookTitle, isbn, grade;
+		int roomNumber,numberbooks;
+		double price;
+		Book.g enumGrade;
 		try {
             // FileReader reads text files in the default encoding.
             FileReader fileReader = 
@@ -32,65 +32,48 @@ public class BookData {
             BufferedReader bufferedReader = 
                 new BufferedReader(fileReader);
             String line="";
-            String[] parts=null;
-            while((line = bufferedReader.readLine()) != null) {
-                parts=line.split("\t");
-                }
-            for (int i=0;i<parts.length;i++){ //Separates parts
-            	String tempString=parts[i];
-            	count=0;
-            	starTemp=tempString.length();
-            	for (int j=tempString.length();j>0;j++){ //one character at a time
-	            	if (tempString.substring(i-1,i)=="/t"){ //Separate isbn, title, etc
-	            		if (count==0){
-	            			isbn=tempString.substring(i,starTemp);
-	            			count++;
-	            			starTemp=i;
-	            		}
-	            		else if (count==1){
-	            			price=Double.parseDouble(tempString.substring(i,starTemp));
-	            			count++;
-	            			starTemp=i;
-	            		}
-	            		else if (count==2){
-	            			numberbooks=Integer.parseInt(tempString.substring(i+1,starTemp));
-	            			count++;
-	            			starTemp=i;
-	            		}
-	            		else if (count==3){
-	            			roomNumber=Integer.parseInt(tempString.substring(i,starTemp));
-	            			count++;
-	            			starTemp=i;
-	            		}
-	            		else if (count==4){ 
-	            			grade=(tempString.substring(i,starTemp));
-	            			count++;
-	            			starTemp=i;
-	            		}
-	            		else{ 
-	            			bookTitle=tempString.substring(0,i);
-	            		}
-	            	}
-            	}
-            	//to change grade to enum
-            	if (grade=="9")
-            		enumGrade=Book.g.NINE;
-            	if (grade=="10")
-            		enumGrade=Book.g.TEN;
-            	if (grade=="11")
-            		enumGrade=Book.g.ELEVEN;
-            	if (grade=="12")
-            		enumGrade=Book.g.TWELVE;
-            	if (grade=="AP")
-            		enumGrade=Book.g.AP;
-            	
-            	BookOrder tempOrder=new BookOrder(roomNumber,numberbooks,"");
-	            Book cycle=new Book(bookTitle, tempOrder, numberbooks, price, isbn, enumGrade); //,int tot,double p,String i,g grd
-	            orig.add(cycle);
-	            }
+            String[] parts={" "};
+            line = bufferedReader.readLine();
+            line = bufferedReader.readLine();
+            line = bufferedReader.readLine();
+			while(line != null) {
+					if(line.charAt(0)!='/'){
+	                    parts=line.split("\t");
+	                    if(parts.length<1){
+	                    	break;
+	                    }
+	                    System.out.println(parts[4]);
+	                    isbn=parts[5];
+	                    if(parts[4].length()==0||parts[4].charAt(0)!='$'){
+	                    	price=Double.parseDouble(parts[4].substring(1,parts[4].length()));
+	                    }else{
+	                    	price=0;
+	                    }
+	                    numberbooks=Integer.parseInt(parts[3]);
+	                    roomNumber=Integer.parseInt(parts[2].split("/")[0]);
+	                    grade=(parts[1]);
+	                    bookTitle=parts[0];
+	                    //to change grade to enum
+		            	if (grade=="9")
+		            		enumGrade=Book.g.NINE;
+		            	else if (grade=="10")
+		            		enumGrade=Book.g.TEN;
+		            	else if (grade=="11")
+		            		enumGrade=Book.g.ELEVEN;
+		            	else if (grade=="12")
+		            		enumGrade=Book.g.TWELVE;
+		            	else
+		            		enumGrade=Book.g.AP;
+		            	
+		            	BookOrder tempOrder=new BookOrder(roomNumber,numberbooks,"");
+			            Book cycle=new Book(bookTitle, tempOrder, numberbooks, price, isbn, enumGrade); //,int tot,double p,String i,g grd
+			            orig.add(cycle);
+					}
+		            line = bufferedReader.readLine();
+            }
 	            
             // Always close files.
-            bufferedReader.close();            
+            bufferedReader.close();
         }
 		
         catch(FileNotFoundException ex) {
