62ad4ad1780a6b9073296dd574aa2eb4922dd89c
isawthehill <sahil.mayenkar@gmail.com>
2015-05-14 10:30:51 -0400

    Update BookWindow.java

--- BookWindow.java
+++ BookWindow.java
@@ -1,19 +1,6 @@
-
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import javax.swing.DefaultListModel;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class BookWindow extends JFrame {
 	/**
