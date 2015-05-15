d7bc1bac893377b53425e803aeb6db50e106025f
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-15 10:26:49 -0400

    Create titleCompare.java

---
+++ titleCompare.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+public class titleCompare implements Comparator<Book> {
+
+	@Override
+	public int compare(Book arg0, Book arg1) {
+		if(arg0.getTitle().equals(arg1.getTitle())){
+			return 0;
+		}
+		return arg0.getTitle().compareTo(arg1.getTitle());
+	}
+}
