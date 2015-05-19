1350b751689f80f40c01381ce86d0f54bb95b564
Wert5 <Wert5@users.noreply.github.com>
2015-05-19 08:51:25 -0400

    Create Sort.java

---
+++ Sort.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+
+public class Sort {
+	public static ArrayList<Book> sortTitle (ArrayList<Book> b){
+		return b;
+	}
+	public static ArrayList<Book> sortPrice (ArrayList<Book> b){
+		return b;
+	}
+	public static ArrayList<Book> sortRoom (ArrayList<Book> b){
+		return b;
+	}
+	public static ArrayList<BookOrder> sortRoomBO (ArrayList<BookOrder> b){
+		return b;
+	}
+	public static ArrayList<Book> sortNum (ArrayList<Book> b){
+		return b;
+	}
+	public static ArrayList<BookOrder> sortNumBO (ArrayList<BookOrder> b){
+		return b;
+	}
+	public static ArrayList<Book> sortISBN (ArrayList<Book> b){
+		return b;
+	}
+	public static ArrayList<Book> sortGrade (ArrayList<Book> b){
+		return b;
+	}
+	public static ArrayList<BookOrder> sortTeacher (ArrayList<BookOrder> b){
+		return b;
+	}
+} 
