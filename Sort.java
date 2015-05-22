36579dda5a7d566858ccda6d7e6c408a7cabe75d
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-22 08:21:52 -0400

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -1,32 +1,41 @@
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 public class Sort {
 	public static ArrayList<Book> sortTitle (ArrayList<Book> b){
+		Collections.sort(b,new titleComparator());
 		return b;
 	}
 	public static ArrayList<Book> sortPrice (ArrayList<Book> b){
+		Collections.sort(b, new priceComparator());
 		return b;
 	}
 	public static ArrayList<Book> sortRoom (ArrayList<Book> b){
+		Collections.sort(b, new roomComparatorBook());
 		return b;
 	}
 	public static ArrayList<BookOrder> sortRoomBO (ArrayList<BookOrder> b){
+		Collections.sort(b, new roomComparatorBookOrder());
 		return b;
 	}
 	public static ArrayList<Book> sortNum (ArrayList<Book> b){
+		Collections.sort(b, new availableCopiesComparator());
 		return b;
 	}
 	public static ArrayList<BookOrder> sortNumBO (ArrayList<BookOrder> b){
+		Collections.sort(b, new availableCopiesComparatorBookOrder());
 		return b;
 	}
 	public static ArrayList<Book> sortISBN (ArrayList<Book> b){
+		Collections.sort(b, new ISBN_Comparator());
 		return b;
 	}
 	public static ArrayList<Book> sortGrade (ArrayList<Book> b){
+		Collections.sort(b, new gradeComparator());
 		return b;
 	}
 	public static ArrayList<BookOrder> sortTeacher (ArrayList<BookOrder> b){
+		Collections.sort(b, new teacherComparator());
 		return b;
 	}
 } 
