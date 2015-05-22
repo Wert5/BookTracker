47e1d56974019af33c6993bbd9d7aa2d7a21f74a
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-22 08:20:44 -0400

    Update and rename roomComparator.java to roomComparatorBookOrder.java

--- roomComparatorBookOrder.java
+++ roomComparatorBookOrder.java
@@ -1,5 +1,5 @@
 import java.util.Comparator;
-public class roomComparator implements Comparator<BookOrder> {
+public class roomComparatorBookOrder implements Comparator<BookOrder> {
 	@Override
 	public int compare(BookOrder arg0, BookOrder arg1) {
 		return (arg0.getRoom())-(arg1.getRoom());
