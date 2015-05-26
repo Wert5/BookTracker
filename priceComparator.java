c045a75de05d41a42e924e225d7e76008565b2f1
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-26 09:58:10 -0400

    Update priceComparator.java

--- priceComparator.java
+++ priceComparator.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
 public class priceComparator implements Comparator<Book> {
 	@Override
 	public int compare(Book arg0, Book arg1) {
-		return (int)((arg0.getPrice()-arg1.getPrice())*100);
+		return (int)((arg0.getPrice()-arg1.getPrice())*100); //Returns a integer representing the difference between the price of the Books to order Books least to greatest based on price
 	}
 }
