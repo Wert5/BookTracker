87bf7bb7830c24fa27120f3e2888c089dd6c72cc
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-26 09:58:25 -0400

    Update roomComparatorBook.java

--- roomComparatorBook.java
+++ roomComparatorBook.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
 public class roomComparatorBook implements Comparator<Book> {
 	@Override
 	public int compare(Book arg0, Book arg1) {
-		return (arg0.getStore().getRoom())-(arg1.getStore().getRoom());
+		return (arg0.getStore().getRoom())-(arg1.getStore().getRoom()); //Returns a integer representing which Book room is smaller to chronologically sort
 	}
 }
