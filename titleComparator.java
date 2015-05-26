3e165815eaeebf5a1c8f2104c800b11d16057e0b
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-26 09:59:07 -0400

    Update titleComparator.java

--- titleComparator.java
+++ titleComparator.java
@@ -1,11 +1,7 @@
 import java.util.Comparator;
 public class titleComparator implements Comparator<Book> {
-
 	@Override
 	public int compare(Book arg0, Book arg1) {
-		if(arg0.getTitle().equals(arg1.getTitle())){
-			return 0;
-		}
-		return arg0.getTitle().compareTo(arg1.getTitle());
+		return arg0.getTitle().compareTo(arg1.getTitle()); //Returns a integer representing which Book title comes first in the alphabet, or if they are identical 
 	}
 }
