98cc5cd864e787a66c90a7c0fc01ef5f8814e1e9
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-26 09:58:55 -0400

    Update teacherComparator.java

--- teacherComparator.java
+++ teacherComparator.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
 public class teacherComparator implements Comparator<BookOrder> {
 	@Override
 	public int compare(BookOrder o1, BookOrder o2) {
-		return (o1.getTeacher()).compareTo(o2.getTeacher());
+		return (o1.getTeacher()).compareTo(o2.getTeacher()); //Returns a integer representing which BookOrder teacher name comes first in the alphabet, or if they are identical;
 	}
 }
