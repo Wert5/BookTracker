e3d2f3f8df13da70b341a75edfdb46c69c51afaa
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-26 09:57:47 -0400

    Update ISBN_Comparator.java

--- ISBN_Comparator.java
+++ ISBN_Comparator.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
 public class ISBN_Comparator implements Comparator<Book> {
 	@Override
 	public int compare(Book arg0, Book arg1) {
-		return (arg0.getIsbn()).compareTo((arg1.getIsbn()));
+		return (arg0.getIsbn()).compareTo((arg1.getIsbn())); //Returns a integer representing which Book ISBN is  to chronologically sort
 	}
 }
