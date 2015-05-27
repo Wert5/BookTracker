d56a0765a5a00efcfd45dca1c36c054b2fc73082
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-27 08:28:05 -0400

    Update availableCopiesComparatorBookOrder.java

--- availableCopiesComparatorBookOrder.java
+++ availableCopiesComparatorBookOrder.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
 public class availableCopiesComparatorBookOrder implements Comparator<BookOrder> {
 	@Override
 	public int compare(BookOrder arg0, BookOrder arg1) {
-		return (arg0.getNum())-(arg1.getNum()); //Returns a integer representing the difference between the number of available copies of the Books and organizes them least to greatest based on number of available copies
+		return (arg0.getNum())-(arg1.getNum()); //Returns a integer representing the difference between the number of available copies of the BookOrders and organizes them least to greatest based on number of available copies
 		}
 }
