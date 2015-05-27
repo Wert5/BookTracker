322acc53477060a7d48e40ac470f0cf195177a10
EshwarKollipara <eshwar.kollipara@gmail.com>
2015-05-27 08:21:34 -0400

    Update gradeComparator.java

--- gradeComparator.java
+++ gradeComparator.java
@@ -2,6 +2,6 @@ import java.util.Comparator;
 public class gradeComparator implements Comparator<Book> {
 	@Override
 	public int compare(Book arg0, Book arg1) {
-		return (arg0.getGrade()).compareTo(arg1.getGrade());
+		return (arg0.getGrade()).compareTo(arg1.getGrade()); //Returns a integer representing the difference between the grade level of the Books to order Books least to greatest based on grade level. Note that AP is the greatest.
 	}
 }
