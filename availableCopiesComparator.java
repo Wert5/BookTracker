4a35ecfced5ddd5c8b5970954574d7eaacf8a42f
Wert5 <Wert5@users.noreply.github.com>
2015-05-29 15:09:04 -0400

    Update availableCopiesComparator.java

--- availableCopiesComparator.java
+++ availableCopiesComparator.java
@@ -1,1 +1,7 @@
-//Returns a integer representing the difference between the number of available copies of the Books and organizes them least to greatest based on number of available copies
+import java.util.Comparator;
+public class availableCopiesComparator implements Comparator<Book> {
+	@Override
+	public int compare(Book arg0, Book arg1) {
+		return (arg0.getStore().getNum())-(arg1.getStore().getNum());
+	}
+}//Returns a integer representing the difference between the number of available copies of the Books and organizes them least to greatest based on number of available copies
